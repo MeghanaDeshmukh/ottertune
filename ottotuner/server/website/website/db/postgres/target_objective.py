f412470205bba55430e4bce01942f02a13eb520e
MeghanaDeshmukh <meghana.j91@gmail.com>
2020-09-16 18:13:59 +0200

    HTAP metrics

--- ottotuner/server/website/website/db/postgres/target_objective.py
+++ ottotuner/server/website/website/db/postgres/target_objective.py
@@ -24,11 +24,9 @@ class ElapsedTime(BaseTargetObjective):
 
 
 class UnifiedHTAPMetric(BaseTargetObjective):
-
     def __init__(self):
         super().__init__(name='Unified_HTAP_metric', pprint='Unified HTAP Metric', unit='seconds',
                          short_unit='s', improvement=MORE_IS_BETTER)
-
     def compute(self, metrics, observation_time):
         variable = metrics['unified_HTAP_metric.QphH'] * metrics['unified_HTAP_metric.tpmC'];
         OLAPWorkers = metrics['unified_HTAP_metric.OLAPWorkers']
@@ -36,8 +34,54 @@ class UnifiedHTAPMetric(BaseTargetObjective):
             return variable/OLAPWorkers
         return variable
 
+class tpmC(BaseTargetObjective):
+    def __init__(self):
+        super().__init__(name='HTAP_tpmC', pprint='HTAP tpmC', unit='seconds',
+                         short_unit='s', improvement=MORE_IS_BETTER)
+    def compute(self, metrics, observation_time):
+        variable = metrics['unified_HTAP_metric.tpmC'];
+        return variable
+
+class QphH(BaseTargetObjective):
+
+    def __init__(self):
+        super().__init__(name='HTAP_QphH', pprint='HTAP QphH', unit='seconds',
+                         short_unit='s', improvement=MORE_IS_BETTER)
+
+    def compute(self, metrics, observation_time):
+        variable = metrics['unified_HTAP_metric.QphH'];
+        return variable
+
+class RankSum_tpmC_QphH(BaseTargetObjective):
+    def __init__(self):
+        super().__init__(name='HTAP_RankSum_tQ', pprint='HTAP RankSum tQ', unit='seconds',
+                         short_unit='s', improvement=MORE_IS_BETTER)
+    def compute(self, metrics, observation_time):
+        ### wi = 2(n+1 - i) / n(n+1);  n: total attributes; i: Rank;
+        ### tpmC: i=0; QphH: i=1;
+        w1 = 2*(3-0)/6;
+        w2 = 2*(3-1)/6;
+        variable = (metrics['unified_HTAP_metric.QphH']*w2) + (w1*metrics['unified_HTAP_metric.tpmC'])
+        return variable
+
+class RankSum_QphH_tpmC(BaseTargetObjective):
+    def __init__(self):
+        super().__init__(name='HTAP_RankSum_Qt', pprint='HTAP RankSum Qt', unit='seconds',
+                         short_unit='s', improvement=MORE_IS_BETTER)
+    def compute(self, metrics, observation_time):
+        ### wi = 2(n+1 - i) / n(n+1);  n: total attributes; i: Rank;
+        ### tpmC: i=0; QphH: i=1;
+        w1 = 2*(3-0)/6;
+        w2 = 2*(3-1)/6;
+        variable = (metrics['unified_HTAP_metric.QphH']*w1) + (w2*metrics['unified_HTAP_metric.tpmC'])
+        return variable
+
 target_objective_list = tuple((DBMSType.POSTGRES, target_obj) for target_obj in [  # pylint: disable=invalid-name
     BaseThroughput(transactions_counter='pg_stat_database.xact_commit'),
     ElapsedTime(),
-    UnifiedHTAPMetric()
+    UnifiedHTAPMetric(),
+    tpmC(),
+    QphH(),
+    RankSum_tpmC_QphH(),
+    RankSum_QphH_tpmC()
 ])
